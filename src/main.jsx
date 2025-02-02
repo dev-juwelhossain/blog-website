9be8fccf5ce5921fcb4d6787c5c32e7ff5f4bd47
dev-juwelhossain <drakj619@gmail.com>
2025-02-02 12:33:23 +0600

    working

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { createBrowserRouter ,
+    RouterProvider,
+    ScrollRestoration,
+    
+  } from'react-router-dom'
+ 
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />,
+  children: [
+    
+  ],
+ 
+}])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}>
+      <App />
+      <ScrollRestoration />
+    </RouterProvider>
+  </StrictMode>
+);
